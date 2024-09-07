f702330805a31212ef9faa45c918681fe6def7fb
TheChandler <ChandlerMiller@gmail.com>
2024-09-07 18:37:41 -0400

    Initial Commit. Not yet a functional node package

---
+++ IShape.ts
@@ -0,0 +1,11 @@
+export interface VectorLike {
+    x: number;
+    y: number;
+}
+export interface IShape {
+    draw: (color: string, offset?: VectorLike) => void
+}
+
+export abstract class Shape{
+    
+}
