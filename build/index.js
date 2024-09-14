65da93abb2b2bc2e789c961b7604193989bfd844
Chandler <ChandlerMiller@gmail.com>
2024-09-13 23:53:52 -0400

    added to build

--- build/index.js
+++ build/index.js
@@ -4,4 +4,6 @@ import { Input } from "./input";
 import { Shape } from "./IShape";
 import { GraphicalButton } from "./menu/GraphicalButton";
 import { ShapeFactory } from "./ShapeFactory";
-export { Polygon, Circle, Shape, ShapeFactory, Sprite, CameraConstructor, Input, GraphicalButton, };
+import { Menu } from "./menu/Menu";
+import { Events } from "./Events";
+export { Polygon, Circle, Shape, ShapeFactory, Sprite, CameraConstructor, Input, GraphicalButton, Menu, Events, };
