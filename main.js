76f9d80b39e7aa02aea9a893b290ac9b68d07a9a
Chandler <chandlermiller@gmail.com>
2025-04-06 21:25:04 -0400

    exported more

--- main.js
+++ main.js
@@ -2,3 +2,7 @@ export * from "./build/GameCamera";
 export * from "./build/State"
 export * from "./build/ShapeFactory"
 export * from "./build/PlayState"
+export * from "./build/GameMath"
+export * from "./buildShapeFactory"
+export * from "./build/input"
+export * from "./build/Audio"
