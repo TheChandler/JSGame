e3fe1573d0dcab4dd86f364ed8222155b829a64e
Chandler <chandlermiller@gmail.com>
2025-04-15 20:16:10 -0400

    fixed main.js

--- main.js
+++ main.js
@@ -1,7 +1,5 @@
 export * from "./build/GameCamera";
 export * from "./build/State"
-export * from "./build/ShapeFactory"
-export * from "./build/PlayState"
 export * from "./build/GameMath"
 export * from "./buildShapeFactory"
 export * from "./build/input"
